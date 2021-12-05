06e9d8f2c1e1a1b2de4d2df2640bb75ea49fe738
Jschaeffer25 <jschaeffer25.chawk.cecil.edu>
2021-12-05 13:17:27 -0500

    Updated Settings

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -23,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3n8owj&e9a%67te8cn7qfw0l9^85_f^jh==(z9ziz#b_-il8y_'
+# SECRET_KEY = 'django-insecure-3n8owj&e9a%67te8cn7qfw0l9^85_f^jh==(z9ziz#b_-il8y_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vertical-joe-site.herokuapp.com']
 
 
 # Application definition
